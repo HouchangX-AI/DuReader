a05d243dbab18dd59dea2d91c6ca5ee171594e61
sunan0519 <0519nan@gmail.com>
2020-04-02 17:57:17 +0800

    first commit

---
+++ run_dureader.sh
@@ -0,0 +1,4 @@
+export DATA_DIR=/env/comprehension/data
+export TASK_NAME=DuReader
+
+python main.py --model_type bert --model_name_or_path bert-base-chinese --task_name $TASK_NAME --do_train --do_eval --data_dir=$DATA_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir ./tmp/$TASK_NAME/
