c0fd285bbc10a1ba99be91563556bd9b1e7a7cd1
sunan0519 <0519nan@gmail.com>
2020-04-21 15:38:10 +0800

    modify shell script

--- run_dureader_roberta.sh
+++ run_dureader_roberta.sh
@@ -1,4 +1,4 @@
 export DATA_DIR=/env/comprehension/data
 export TASK_NAME=DuReader
-export CUDA_VISIBLE_DEVICES=0,2,3
-python main.py --model_type bert --model_name_or_path chinese_wwm_ext_pytorch --task_name $TASK_NAME --do_train --do_eval --data_dir=$DATA_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir ./roberta_output/$TASK_NAME/
+export CUDA_VISIBLE_DEVICES=2,3
+python main.py --model_type bert --model_name_or_path chinese_wwm_ext_pytorch --task_name $TASK_NAME --do_train --evaluate_during_training --data_dir=$DATA_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 10.0 --output_dir ./roberta_output/$TASK_NAME/ --overwrite_output_dir --overwrite_cache
